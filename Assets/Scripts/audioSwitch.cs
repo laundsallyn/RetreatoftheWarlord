77b2c53256f4e00d8d9646fb6e25726e49ec99c7
Yipu Wang <yipu.wang@utexas.edu>
2016-07-17 01:25:25 -0500

    have a global sound control with saved state, toggle always reset to checked

--- Assets/Scripts/audioSwitch.cs
+++ Assets/Scripts/audioSwitch.cs
@@ -1,15 +1,73 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+// Toggles always start with checked!
 public class audioSwitch : MonoBehaviour {
+    Toggle BGMtog,SFXtog;
+    Scene now;
+    void Awake()
+    {
+        bool inSettings = now.name == "Settings";
+        now = SceneManager.GetActiveScene();
+        if(inSettings)
+        {
+            BGMtog = GameObject.FindGameObjectWithTag("BGM").GetComponent<Toggle>();
+            SFXtog = GameObject.FindGameObjectWithTag("SFX").GetComponent<Toggle>();
+        }
 
-    // Use this for initialization
 
+        //Debug.Log("This is BGMtog");
+        //Debug.Log("This is SFXtog");
 
-    public void audioControl () {
-        if (AudioListener.pause)
+        if (PlayerPrefs.GetInt("BGM") == 1)
+        {
+            if(inSettings)
+                BGMtog.isOn = false;
             AudioListener.pause = false;
-        else
+        }
+        else {
+            if (inSettings)
+                BGMtog.isOn = false;
             AudioListener.pause = true;
+
+        }
+
+        //still need to add controls for SFX
+        if (PlayerPrefs.GetInt("SFX") == 1)
+            if (inSettings)
+                SFXtog.isOn = true;
+        else
+            if (inSettings)
+                SFXtog.isOn = false;
     }
+
+    public void BGMControl ()
+    {
+        if (AudioListener.pause)
+        {
+            AudioListener.pause = false;
+            PlayerPrefs.SetInt("BGM", 1);
+        }
+        else
+        {
+            AudioListener.pause = true;
+            PlayerPrefs.SetInt("BGM", 0);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void SFXControl()
+    {
+        if (SFXtog.isOn)
+        {
+            PlayerPrefs.SetInt("SFX", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("SFX", 0);
+
+        }
+    }
+
 }
---
+++ Assets/Scripts/restoreToggleState.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class restoreToggleState : MonoBehaviour
+{
+    
+    public void restoreState(string toggleName)
+    {
+        Toggle tog = GameObject.Find(toggleName).GetComponent<Toggle>();
+        if (PlayerPrefs.GetInt("mute") == 1)
+            tog.isOn = false;
+        else
+            tog.isOn = true;
+            
+    }
+}
