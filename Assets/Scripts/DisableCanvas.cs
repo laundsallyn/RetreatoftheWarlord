ba1e367d0b0a5f867e6fb2a7a8f4d7ab8ce04a14
Yipu Wang <yipu.wang@utexas.edu>
2016-08-02 18:00:15 -0500

    in game pause implemented, still need to change code in EnemyCode.cs

---
+++ Assets/Scripts/DisableCanvas.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableCanvas : MonoBehaviour {
+    public GameObject screen;
+
+	public void resume()
+    {
+        screen.SetActive(false);
+        PlayerPrefs.SetInt("Pause", 0);
+        PlayerPrefs.Save();
+    }
+    public void pauseGame()
+    {
+        print("Pause Button Clicked");
+        screen.SetActive(true);
+        PlayerPrefs.SetInt("Pause", 1);
+        PlayerPrefs.Save();
+    }
+    void Awake()
+    {
+        print("Awake is called");
+        screen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (paused)
+    //        screen.SetActive(false);
+    //    else
+    //        screen.SetActive(true);
+
+    //}
+}
