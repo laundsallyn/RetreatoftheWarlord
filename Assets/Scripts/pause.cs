afffa79a50700ef0042c8fc11a65116813ef28d1
Yipu Wang <yipu.wang@utexas.edu>
2016-08-01 20:37:03 -0500

    added pause button

---
+++ Assets/Scripts/pause.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pause : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    public void pauseGame()
+    {
+        if (Time.timeScale == 1.0F)
+            Time.timeScale = 0F;
+        else
+            Time.timeScale = 1.0F;
+    }
+}
+
