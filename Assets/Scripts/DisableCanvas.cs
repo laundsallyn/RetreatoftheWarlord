8d6a8fec56cbd47f2686a96b8f47c300b3d59b13
Zin20 <jimmyne@gmail.com>
2016-08-06 15:09:10 -0500

    Having issues with audio

--- Assets/Scripts/DisableCanvas.cs
+++ Assets/Scripts/DisableCanvas.cs
@@ -4,19 +4,25 @@ using System.Collections;
 public class DisableCanvas : MonoBehaviour {
     public GameObject screen;
 
+
 	public void resume()
-    {
+    {     
+        print("Resuming");
+
         screen.SetActive(false);
         PlayerPrefs.SetInt("Pause", 0);
         PlayerPrefs.Save();
         GameObject.Find("EnemyPoints").GetComponent<enemySpawnBehavior>().invokeRepeat();
+        Time.timeScale= 1;
     }
     public void pauseGame()
-    {
+    {     
+
         print("Pause Button Clicked");
         screen.SetActive(true);
         PlayerPrefs.SetInt("Pause", 1);
         PlayerPrefs.Save();
+        Time.timeScale= 0;
     }
     void Awake()
     {
